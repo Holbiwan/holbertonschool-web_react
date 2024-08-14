d7b3edb57ff132f72459ea642c6dc0fb6d9e99d8
Holbiwan <sabrinapapeau@gmail.com>
2024-08-14 06:31:16 +0200

    Update file

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -1,3 +1,4 @@
+// Declaration of the Student interface with the required properties
 interface Student {
     first_name: string;
     name: string;
@@ -5,6 +6,7 @@ interface Student {
     location: string;
 }
 
+// Creating the first student object according to the Student interface
 const student1: Student = {
     first_name: 'Teddy',
     name: 'Riner',
@@ -12,41 +14,62 @@ const student1: Student = {
     location: 'Paris',
 };
 
+// Creating the second student object according to the Student interface
 const student2: Student = {
     first_name: 'Rafael',
-    lastName: 'Nadal',
-    age:    38,
+    lastName: 'Nadal', // Error: Incorrect use of 'lastName' instead of 'name'
+    age: 38,
+    location: 'Manacor',
+};
+
+// Correction: Renaming 'lastName' to 'name' to match the Student interface
+const student2: Student = {
+    first_name: 'Rafael',
+    name: 'Nadal',
+    age: 38,
     location: 'Manacor',
 }; 
 
+// Creating a list of students containing the two student objects defined above
 const studentsList: Array<Student> = [student1, student2];
-const labels: string[] = ["firstName", "location"];
 
+// Defining the labels for the table columns
+const labels: string[] = ["first_name", "location"];
+
+// Creating an HTML table element
 const table: HTMLTableElement = document.createElement("table");
+
+// Creating table sections: thead for the header and tbody for the body
 const thead: HTMLTableSectionElement = document.createElement("thead");
 const tbody: HTMLTableSectionElement = document.createElement("tbody");
 
+// Appending the table to the HTML document body
 document.body.appendChild(table);
 table.appendChild(thead);
 table.appendChild(tbody);
 
+// Adding column headers to the table using the defined labels
 for (let i: number = 0; i < labels.length; i++) {
-	const th: HTMLTableCellElement = document.createElement("th");
-	th.appendChild(document.createTextNode(`${labels[i]}`));
-	thead.appendChild(th);
+    const th: HTMLTableCellElement = document.createElement("th");
+    th.appendChild(document.createTextNode(`${labels[i]}`));
+    thead.appendChild(th);
 }
 
+// Adding the students' data to the table
 for (let i: number = 0; i < studentsList.length; i++) {
-	const tr: HTMLTableRowElement = document.createElement("tr");
-	tbody.appendChild(tr);
-	const values: string[] = [
-		studentsList[i].firstName,
-		studentsList[i].location,
-	];
+    const tr: HTMLTableRowElement = document.createElement("tr");
+    tbody.appendChild(tr);
+    
+    // Extracting specific values to display (first_name and location)
+    const values: string[] = [
+        studentsList[i].first_name,
+        studentsList[i].location,
+    ];
 
-	for (let j: number = 0; j < values.length; j++) {
-		const td: HTMLTableCellElement = document.createElement("td");
-		td.appendChild(document.createTextNode(`${values[j]}`));
-		tr.appendChild(td);
-	}
+    // Adding the values to the table row
+    for (let j: number = 0; j < values.length; j++) {
+        const td: HTMLTableCellElement = document.createElement("td");
+        td.appendChild(document.createTextNode(`${values[j]}`));
+        tr.appendChild(td);
+    }
 }
