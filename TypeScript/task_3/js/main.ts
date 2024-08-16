b561a0705a4cfd00c08e09acb2648d41bb7abf7b
Holbiwan <sabrinapapeau@gmail.com>
2024-08-16 09:09:36 +0200

    Review all files

--- TypeScript/task_3/js/main.ts
+++ TypeScript/task_3/js/main.ts
@@ -1,29 +1,19 @@
-/// <reference path="./crud.d.ts" />
+/// <reference path ='./crud.d.ts' />
 
-// Import types from interface.ts
 import { RowID, RowElement } from './interface';
-
-// Import everything from crud.js as CRUD
 import * as CRUD from './crud';
 
-// Create a RowElement object
 const row: RowElement = {
-    firstName: 'Guillaume',
-    lastName: 'Salva'
+  firstName: 'Guilaume',
+  lastName: 'Salva',
 };
 
-// Insert the row and get the new row ID
 const newRowID: RowID = CRUD.insertRow(row);
-
-// Create an updated row with an additional age field
-const updatedRow: RowElement = {
-    firstName: 'Guillaume',
-    lastName: 'Salva',
-    age: 23
+const updatedRow: RowElement = { 
+  firstName: 'Guilaume',
+  lastName: 'Salva',
+  age: 23
 };
 
-// Update the row with the new data
 CRUD.updateRow(newRowID, updatedRow);
-
-// Delete the row by its ID
 CRUD.deleteRow(newRowID);
