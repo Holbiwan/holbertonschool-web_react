9f8dde772b51017336d2537145237e371080ce68
Holbiwan <sabrinapapeau@gmail.com>
2024-08-16 09:54:20 +0200

    Fix version conflict

--- TypeScript/webpack.config.js
+++ TypeScript/webpack.config.js
@@ -8,7 +8,9 @@ module.exports = {
   },
   mode: 'development',
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
     compress: true,
     port: 9000,
   },
