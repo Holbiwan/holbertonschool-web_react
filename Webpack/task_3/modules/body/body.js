a31733d9c844a3bee670fab23244de03f3684cf9
Holbiwan <sabrinapapeau@gmail.com>
2024-08-17 10:11:09 +0200

    Divide code into modules & optimize bundling with Webpack.

--- Webpack/task_3/modules/body/body.js
+++ Webpack/task_3/modules/body/body.js
@@ -2,15 +2,19 @@ import $ from 'jquery';
 import _ from 'lodash';
 import './body.css';
 
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
 
-let counter = 0;
+$(() => {
+  $('body').append(`
+    <button>Click here to get started</button>
+    <p id='count'></p>
+  `);
 
-const updateCounter = () => {
-  counter++;
-  $('#count').html(`${counter} clicks on the button`);
-};
+  let count = 0;
+  function updateCounter() {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+  }
 
-$('button').on('click', _.debounce(updateCounter, 500));
+  const debouncedUpdateCounter = _.debounce(updateCounter, 500);
+  $('button').click(debouncedUpdateCounter);
+})
--- Webpack/task_3/modules/header/header.js
+++ Webpack/task_3/modules/header/header.js
@@ -1,9 +1,9 @@
 import $ from 'jquery';
-
 import './header.css';
 
-$('body').append('<header></header>');
-$('header').append('<div id="logo"></div>',
-                   '<h1>Holberton Dashboard</h1>');
+$(() => {
+    $('body').append('<div id="logo"></div>');
+    $('body').append('<h1>Holberton Dashboard</h1>');
 
-console.log('Init header');
+    console.log('Init header');
+})
