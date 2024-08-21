01eb571fdb65d71d81279551c92c754f30c5dba1
Holbiwan <sabrinapapeau@gmail.com>
2024-08-21 19:47:21 +0200

    Add getFullYear & getFooterCopy functions

--- react_intro/task_1/dashboard/src/utils.js
+++ react_intro/task_1/dashboard/src/utils.js
@@ -1,7 +1,11 @@
+// task_1/dashboard/src/utils.js
+
+// Fonction pour obtenir l'année en cours
 export function getFullYear() {
-    return new Date().getFullYear();
-  }
-  
-  export function getFooterCopy(isIndex) {
-    return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
-  }
+  return new Date().getFullYear();
+}
+
+// Fonction pour obtenir le texte du footer
+export function getFooterCopy(isIndex) {
+  return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+}
