9ab9f340baa59cb8568231dc9732ec5d2e4cdf87
Holbiwan <sabrinapapeau@gmail.com>
2024-08-23 15:01:26 +0200

    Review all code and structure

--- holbertonschool-web_react/react_intro/task_0/dashboard/src/App.js
+++ holbertonschool-web_react/react_intro/task_0/dashboard/src/App.js
@@ -1,21 +1,12 @@
-import React from "react";
-import "./App.css";
-import holberton_logo from "./holberton_logo.jpg";
+import React from 'react';
 
 function App() {
   return (
-    <>
-      <div className="App-header">
-        <img src={holberton_logo} />
-        <h1>School dashboard</h1>
-      </div>
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-      </div>
-      <div className="App-footer">
-        <p>Copyright 2020 - holberton School</p>
-      </div>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <h1>Bienvenue sur le tableau de bord</h1>
+      </header>
+    </div>
   );
 }
 
