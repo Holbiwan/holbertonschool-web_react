afc0b69576af14ae5bacacb5a8064d167bb0373b
Holbiwan <sabrinapapeau@gmail.com>
2024-08-24 04:30:09 +0200

    Handle modifications

---
+++ react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { getFullYear, getFooterCopy } from './utils';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="holberton_logo.jpg" alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+        <br />
+        <button>OK</button>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
