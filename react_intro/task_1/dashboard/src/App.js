3e5e6740bec64b27cfa88b86d6fc20e94c6fd0bc
Holbiwan <sabrinapapeau@gmail.com>
2024-08-24 04:28:25 +0200

    Create functions and notifications

---
+++ react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { getFullYear, getFooterCopy } from './utils';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="holberton_logo.jpg" alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
