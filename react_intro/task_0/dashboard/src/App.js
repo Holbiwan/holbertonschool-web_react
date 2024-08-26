c8076d1b235032725b31d643844c40c6b181f568
Holbiwan <sabrinapapeau@gmail.com>
2024-08-26 17:19:12 +0200

    Update App.js and App.css files

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import logo from './HolbertonLogo.png';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="Holberton logo" />
         <h1>School dashboard</h1>
       </header>
-      <div className="App-body">
+      <body className="App-body">
         <p>Login to access the full dashboard</p>
-      </div>
+      </body>
       <footer className="App-footer">
         <p>Copyright 2024 - Holberton School</p>
       </footer>
     </div>
   );
 }
-
 export default App;
