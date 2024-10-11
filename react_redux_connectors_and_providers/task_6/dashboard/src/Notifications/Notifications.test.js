3d33bf2856d3b37131b24c351b152c95d40a3f32
Holbiwan <sabrinapapeau@gmail.com>
2024-10-11 14:47:11 +0200

    Modify notifications connectors

--- react_redux_connectors_and_providers/task_6/dashboard/src/Notifications/Notifications.test.js
+++ react_redux_connectors_and_providers/task_6/dashboard/src/Notifications/Notifications.test.js
@@ -7,6 +7,7 @@ import notificationsNormalizer from "../schema/notifications";
 import { Map, fromJS } from "immutable";
 import { getUnreadNotifications } from "../selectors/notificationSelector";
 
+// const NOTIFICATIONS
 const NOTIFICATIONS = [
   {
     id: "5debd76480edafc8af244228",
